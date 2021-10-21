d34fc9155a31042285784bef692540630ba387e4
Vaughan Allan <vornstar@gmail.com>
2021-10-21 21:55:08 +0100

    Added update message

--- src/github_oauth/custom_user/views.py
+++ src/github_oauth/custom_user/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth import get_user_model
+from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import UpdateView
 
 
-class UserProfileView(UpdateView):
+class UserProfileView(SuccessMessageMixin, UpdateView):
     model = get_user_model()
     fields = [
         "first_name",
@@ -13,6 +14,7 @@ class UserProfileView(UpdateView):
         "prev_address",
     ]
     success_url = "/myprofile"
+    success_message = "Profile updated."
 
     def get_object(self, queryset=None):
         return self.request.user
