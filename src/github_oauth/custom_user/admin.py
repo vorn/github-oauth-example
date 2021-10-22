ec1b6322c05949f693eb3a07dc897e848c506994
Vaughan Allan <vornstar@gmail.com>
2021-10-22 11:05:49 +0100

    Added CustomUser to admin site

--- src/github_oauth/custom_user/admin.py
+++ src/github_oauth/custom_user/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.admin import ModelAdmin
+
+from github_oauth.custom_user.models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(ModelAdmin):
+    pass
